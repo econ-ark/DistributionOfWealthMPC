93c7d77fd00a823111610c4b49bc23d3962b2fa2
Cameron Riddell <cameron@dutc.io>
2024-05-30 14:19:58 -0700

    remove install from reproduce

--- reproduce.sh
+++ reproduce.sh
@@ -1,15 +1,3 @@
-#!/bin/bash
-# DistributionOfWealth reproduce
+#!/usr/bin/env bash
 
-if ! command -v conda &> /dev/null
-then
-    echo "`conda` (anaconda) needs to be installed to reproduce!"
-    exit
-fi
-
-conda env update -f binder/environment.yml --prefix ./condaenv/
-
-conda_root=$(conda info -s | grep CONDA_ROOT | sed 's/CONDA_ROOT\: //')
-source $conda_root/bin/activate ./condaenv
-
-ipython do_min.py
+PYTHONPATH=. ipython --classic --matplotlib=agg do_min.py
